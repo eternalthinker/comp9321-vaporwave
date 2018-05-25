324e5179ea14d4f83f9c38cc8eb74595a34a3533
Rahul Anand <rahulanand.inbox@gmail.com>
2018-05-26 03:24:56 +1000

    Raw query in IMDB service for episodeCount

--- imdb/app/controller.py
+++ imdb/app/controller.py
@@ -2,6 +2,7 @@ from flask import jsonify
 
 from app import app
 from app.models import *
+from app.database import engine
 
 
 def seid(sid, eid):
@@ -10,12 +11,60 @@ def seid(sid, eid):
     return sid + eid
 
 
+@app.route('/episodes', methods=['GET'])
+def all_episodes():
+    eps = Episode.query.all()
+    episodes = []
+    for e in eps:
+        e = dict(e.__dict__)
+        e.pop('_sa_instance_state')
+        e['averageRating'] = str(e['averageRating'])[:3]
+        episodes.append(e)
+    return jsonify(episodes), 200
+
+
+@app.route('/season/<season>/episode/<episode>/characters', methods=['GET'])
+def test(season, episode):
+    cs = engine.execute('''
+        select c.CID, name, actor, season_of_death, episode_of_death, means_of_death, episodeCount,
+            CASE WHEN 
+                cast(season_of_death as integer) == {} and cast(episode_of_death as integer) == {} 
+            THEN cast(0 as bit) ELSE cast(1 as bit) END as isAlive
+        from characters as c, episodes as e, episode_characters as ec,
+            (select c1.CID, count(*) as episodeCount 
+             from episodes as e1, characters as c1, episode_characters as ec1
+             where 
+                cast(e1.seasonNumber as integer) <= {} and
+                cast(e1.episodeNumber as integer) <= {} and
+                e1.EID == ec1.EID and
+                c1.CID == ec1.CID
+                group by c1.CID
+            ) as eCounts
+        where 
+            cast(seasonNumber as integer) == {} and 
+            cast(episodeNumber as integer) == {} and
+            e.EID == ec.EID and
+            c.CID == ec.CID and
+            c.CID == eCounts.CID
+        ;
+        '''.format(season, episode, season, episode, season, episode)
+    )
+    characters = []
+    for character in cs:
+        character_dict = {}
+        for pair in character.items():
+            character_dict = {**character_dict, **{pair[0]: pair[1]}}
+        characters.append(character_dict)
+    return jsonify(characters), 200
+
+
 @app.route('/season/<sid>/episode/<eid>', methods=['GET'])
 def episode(sid, eid):
     e = Episode.query.filter(Episode.EID == seid(sid, eid))[0]
     if e:
         e = dict(e.__dict__)
         e.pop('_sa_instance_state')
+        e['averageRating'] = str(e['averageRating'])[:3]
         return jsonify(e), 200
     return "Episode Not Found", 404
 
@@ -30,7 +79,7 @@ def character(cid):
     return "Character Not Found", 404
 
 
-@app.route('/season/<sid>/episode/<eid>/characters', methods=['GET'])
+@app.route('/season/<sid>/episode/<eid>/characters_old', methods=['GET'])
 def episode_characters(sid, eid):
     ceid = seid(sid, eid)
     cs = Character.query.outerjoin(EpisodeCharacters).filter(EpisodeCharacters.EID == ceid)
