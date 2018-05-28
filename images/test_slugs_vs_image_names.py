add44b8014dc1382ab1db101399b5ead3cb331cb
Daniel Kennedy <dannykennedy@email.com>
2018-05-28 20:03:02 +1000

    more images

--- images/test_slugs_vs_image_names.py
+++ images/test_slugs_vs_image_names.py
@@ -5,6 +5,7 @@ import urllib
 img_files = os.listdir("./static")
 img_file_slugs = []
 slugs = []
+iaf_slugs = []
 
 # Get slugs from img files
 for img in img_files:
@@ -12,17 +13,23 @@ for img in img_files:
 	img = img.lower()
 	img_file_slugs.append(img)
 
-cid_file = open("CIDs.txt", 'r')
+# Get slugs from IaF
+iaf_file = open("iaf_slugs.txt", 'r')
+for line in iaf_file:
+	iaf_slugs.append(line.strip())
 
+# Get slugs from IMDB
+cid_file = open("CIDs.txt", 'r')
 for line in cid_file:
 	slugs.append(line.strip())
 
-print("testing if image slugs exist in db")
-for slug in img_file_slugs:
-	if not slug in slugs:
-		print(slug)
+# print("testing if image slugs exist in db-------------")
+# for slug in img_file_slugs:
+# 	if not slug in slugs:
+# 		print(slug)
 
-print("testing if db slugs exist in image files")
+print("testing if db slugs have images-------------")
 for slug in slugs:
-	if not slug in img_file_slugs:
-		print(slug)
+	if slug in iaf_slugs:
+		if not slug in img_file_slugs:
+			print(slug)
